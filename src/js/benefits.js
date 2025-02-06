748ce5c5f28c574f1e1688bff344ea35802bc67b
Liberlay <61653177+Liberlay@users.noreply.github.com>
2025-02-06 18:50:32 +0000

    Deploying to gh-pages from @ Liberlay/project_team_js_11@58ad24f0a97750edc3ceec32aee7388892c7f652 🚀

--- src/js/benefits.js
+++
@@ -1,6 +0,0 @@
-const benefitsButton = document.querySelector('.benefits-button');
-const footer = document.getElementById('work-together');
-
-benefitsButton.addEventListener('click', () => {
-    footer.scrollIntoView({ behavior: 'smooth' });
-});
