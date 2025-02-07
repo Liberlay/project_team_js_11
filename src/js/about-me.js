942d02b7430ac754cbf0cd8fd72a74e035146123
Evgen Fedorchenko <135177643+NayxieX@users.noreply.github.com>
2025-02-07 23:21:23 +0200

    Code update with all animations and picture resp

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,33 +1,28 @@
 // Evgen Fedorchenko
 
 // Accordion
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
 
-// document.querySelectorAll('.about-me-button-down').forEach(button => {
-//   button.addEventListener('click', () => {
-//     const panel = button.closest('li').querySelector('.ac-panel');
-//     const isOpen = panel.classList.contains('open');
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-//     document.querySelectorAll('.ac-panel').forEach(otherPanel => {
-//       otherPanel.classList.remove('open');
-//       otherPanel.previousElementSibling
-//         .querySelector('.about-me-button-down')
-//         .classList.remove('open');
-//     });
+// Css svg rotator
 
-//     if (!isOpen) {
-//       panel.classList.add('open');
-//       button.classList.add('open');
-//     }
-//   });
-// });
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.querySelector(
+    '.accordion-container-about .ac-trigger'
+  );
+  const parent = button.closest('.ac'); // Родительский элемент с классом "ac"
 
-// const accordionAbout = new Accordion('accordion-container-about', {
-//   duration: 700,
-//   showMultiple: true,
-// });
-// accordionAbout.open(0);
+  button.addEventListener('click', function () {
+    parent.classList.toggle('is-rotated'); // Добавляет/убирает класс "is-active"
+  });
+});
+// Css svg rotator
+
+const accordionAbout = new Accordion('.accordion-container-about', {
+  duration: 700,
+});
+accordionAbout.open(0);
 
 // Evgen Fedorchenko
 
